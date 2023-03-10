2921474312d0d4c7952656ca8e365a6baf1fe0eb
Kiril Shenouda Khalil <kiril.shenouda-khalil@nmi.fi>
2023-03-10 14:11:37 +0200

    Split tutorial to own script and added Text vectorization layer model to main.py.

--- main.py
+++ main.py
@@ -1,147 +1,31 @@
-import tensorflow as tf
+import pandas as pd
 import numpy as np
-
-
-batch_size = 32
-raw_train_ds = tf.keras.utils.text_dataset_from_directory(
-    "../aclImdb/train",
-    batch_size=batch_size,
-    validation_split=0.2,
-    subset="training",
-    seed=1337,
-)
-raw_val_ds = tf.keras.utils.text_dataset_from_directory(
-    "../aclImdb/train",
-    batch_size=batch_size,
-    validation_split=0.2,
-    subset="validation",
-    seed=1337,
-)
-raw_test_ds = tf.keras.utils.text_dataset_from_directory(
-    "../aclImdb/test", batch_size=batch_size
-)
-
-print(f"Number of batches in raw_train_ds: {raw_train_ds.cardinality()}")
-print(f"Number of batches in raw_val_ds: {raw_val_ds.cardinality()}")
-print(f"Number of batches in raw_test_ds: {raw_test_ds.cardinality()}")
-
-for text_batch, label_batch in raw_train_ds.take(1):
-    for i in range(5):
-        print(text_batch.numpy()[i])
-        print(label_batch.numpy()[i])
-
-
-from tensorflow.keras.layers import TextVectorization
-import string
-import re
-
-
-def custom_standardization(input_data):
-    lowercase = tf.strings.lower(input_data)
-    stripped_html = tf.strings.regex_replace(lowercase, "<br />", " ")
-    return tf.strings.regex_replace(
-        stripped_html, f"[{re.escape(string.punctuation)}]", ""
-    )
-
-
-max_features = 20000
-embedding_dim = 128
-sequence_length = 500
-
-
-vectorize_layer = TextVectorization(
-    standardize=custom_standardization,
-    max_tokens=max_features,
-    output_mode="int",
-    output_sequence_length=sequence_length,
-)
-
-
-# Let's make a text-only dataset (no labels):
-text_ds = raw_train_ds.map(lambda x, y: x)
-# Let's call `adapt`:
-vectorize_layer.adapt(text_ds)
-
-def vectorize_text(text, label):
-    text = tf.expand_dims(text, -1)
-    return vectorize_layer(text), label
-
-
-# Vectorize the data.
-train_ds = raw_train_ds.map(vectorize_text)
-val_ds = raw_val_ds.map(vectorize_text)
-test_ds = raw_test_ds.map(vectorize_text)
-
-# Do async prefetching / buffering of the data for best performance on GPU.
-train_ds = train_ds.cache().prefetch(buffer_size=10)
-val_ds = val_ds.cache().prefetch(buffer_size=10)
-test_ds = test_ds.cache().prefetch(buffer_size=10)
-
-
 from tensorflow.keras import layers
 
-# A integer input for vocab indices.
-inputs = tf.keras.Input(shape=(None,), dtype="int64")
+# Make numpy values easier to read.
+np.set_printoptions(precision=3, suppress=True)
 
-# Next, we add a layer to map those vocab indices into a space of dimensionality
-# 'embedding_dim'.
-x = layers.Embedding(max_features, embedding_dim)(inputs)
-x = layers.Dropout(0.5)(x)
+import tensorflow as tf
+from tensorflow.keras import layers
 
-# Conv1D + global max pooling
-x = layers.Conv1D(128, 7, padding="valid", activation="relu", strides=3)(x)
-x = layers.Conv1D(128, 7, padding="valid", activation="relu", strides=3)(x)
-x = layers.GlobalMaxPooling1D()(x)
+talo_train = pd.read_csv('../talo_train_data.csv')
+talo_np = np.array(talo_train)
+print(talo_np)
 
-# We add a vanilla hidden layer:
-x = layers.Dense(128, activation="relu")(x)
-x = layers.Dropout(0.5)(x)
-
-# We project onto a single unit output layer, and squash it with a sigmoid:
-predictions = layers.Dense(1, activation="sigmoid", name="predictions")(x)
-
-model = tf.keras.Model(inputs, predictions)
-
-# Compile the model with binary crossentropy loss and an adam optimizer.
-model.compile(loss="binary_crossentropy", optimizer=tf.keras.optimizers.legacy.Adam(learning_rate=1e-3), metrics=["accuracy"])
-
-"""
-## Train the model
-"""
-
-epochs = 3
-
-# Fit the model using the train and test datasets.
-history = model.fit(train_ds, validation_data=val_ds, epochs=epochs)
-
-print(history.history)
-"""
-## Evaluate the model on the test set
-"""
-
-results = model.evaluate(test_ds)
-print("test loss, test acc:", results)
-
-
-"""
-## Make an end-to-end model
-If you want to obtain a model capable of processing raw strings, you can simply
-create a new model (using the weights we just trained):
-"""
-
-# A string input
-inputs = tf.keras.Input(shape=(1,), dtype="string")
-# Turn strings into vocab indices
-indices = vectorize_layer(inputs)
-# Turn vocab indices into predictions
-outputs = model(indices)
-
-# Our end to end model
-end_to_end_model = tf.keras.Model(inputs, outputs)
-end_to_end_model.compile(
-    loss="binary_crossentropy", optimizer=tf.keras.optimizers.legacy.Adam(learning_rate=1e-3), metrics=["accuracy"]
+vectorize_layer = layers.TextVectorization(
+    standardize="lower_and_strip_punctuation",
+    split="character",
+    output_mode="int",
 )
 
-# Test it with `raw_test_ds`, which yields raw strings
-results2 = end_to_end_model.evaluate(raw_test_ds)
-print("test loss2, test acc2:", results2)
+vectorize_layer.adapt(talo_np)
+
+model = tf.keras.models.Sequential()
+model.add(tf.keras.Input(shape=(1,), dtype=tf.string))
+model.add(vectorize_layer)
+model.add(tf.keras.layers.Embedding(1000, 64, input_length=10))
+model.compile('rmsprop', 'mse')
+
+
+test = model.predict(talo_train)
+print(test.shape)
