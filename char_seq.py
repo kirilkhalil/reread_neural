aeb5f438e72bcdc1e6c21aec452a50439ad1d9b2
Kiril Shenouda Khalil <kiril.shenouda-khalil@nmi.fi>
2023-03-31 10:18:46 +0300

    RD: RNN example.

--- char_seq.py
+++ char_seq.py
@@ -33,14 +33,14 @@ def save_doc(lines, filename):
 
 
 # load text
-raw_text = load_doc('../talo_words.txt')
+raw_text = load_doc('../poem.txt')
 
 # clean
 tokens = raw_text.split()
 raw_text = ' '.join(tokens)
 
 # organize into sequences of characters
-length = 7
+length = 10
 sequences = list()
 for i in range(length, len(raw_text)):
     # select sequence of tokens
@@ -63,33 +63,35 @@ chars = sorted(list(set(raw_text)))
 # chars contains line change and \ufeff values so we remove those as we don't want to map those.
 chars.remove('\n')
 mapping = dict((c, i) for i, c in enumerate(chars))
-
 sequences = list()
 for line in lines:
     encoded_seq = [mapping[char] for char in line]
     sequences.append(encoded_seq)
 
 vocab_size = len(mapping)
-
 # Last element is empty due to using txt file and weird encoding stuff, so we pop it for now to make life easier.
 sequences.pop()
+print(sequences)
 sequences = np.array(sequences)
+print(sequences)
+#print(sequences)
 X, y = sequences[:, :-1], sequences[:, -1]
-
 sequences = [to_categorical(x, num_classes=vocab_size) for x in X]
+print(sequences)
 X = np.array(sequences)
+print(X)
 y = to_categorical(y, num_classes=vocab_size)
 
-model = Sequential()
-model.add(LSTM(75, input_shape=(X.shape[1], X.shape[2])))
-model.add(Dense(vocab_size, activation='softmax'))
-print(model.summary())
-model.compile(loss='categorical_crossentropy', optimizer=tf.keras.optimizers.legacy.Adam(learning_rate=1e-3),
-              metrics=['accuracy'])
-model.fit(X, y, epochs=110, verbose=2)
-
-
-# save the model to file
-model.save('lstm_auto_enc.h5')
-# save the mapping
-dump(mapping, open('mapping.pkl', 'wb'))
+#model = Sequential()
+#model.add(LSTM(75, input_shape=(X.shape[1], X.shape[2])))
+#model.add(Dense(vocab_size, activation='softmax'))
+#print(model.summary())
+#model.compile(loss='categorical_crossentropy', optimizer=tf.keras.optimizers.legacy.Adam(learning_rate=1e-3),
+#              metrics=['accuracy'])
+#model.fit(X, y, epochs=110, verbose=2)
+#
+#
+## save the model to file
+#model.save('lstm_auto_enc.h5')
+## save the mapping
+#dump(mapping, open('mapping.pkl', 'wb'))
