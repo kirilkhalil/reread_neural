2e4117f040003e333ce3f5d0bcc70d4fb961316c
Kiril Shenouda Khalil <kiril.shenouda-khalil@nmi.fi>
2023-03-31 11:00:21 +0300

    RD: Model that learns and outputs a vector the length of the input instances. When predicting with a single input belonging to the corpus it would seem that the correct index of the output vector has the highest value.
    Using softmax here with 500 inputs so the sum of the 500 output index values should equate to 1. Not sure if it's normal that even the highest value is in the range of 0.03... in such a case.

--- feedforward.py
+++ feedforward.py
@@ -25,10 +25,6 @@ mapping = dict((c, i) for i, c in enumerate(chars))  # All input chars given an 
 vocab_size = len(mapping)  # Size of vocabulary
 print(vocab_size)
 
-#sequences = list()  # Words transposed into integer coding based on defined char values
-#for line in lines:
-#    encoded_seq = [mapping[char] for char in line]
-#    sequences.append(encoded_seq)
 
 vectorize_layer = layers.TextVectorization(
     standardize=None,
@@ -41,25 +37,14 @@ vectorize_layer2 = layers.TextVectorization(
     output_mode="int",
 )
 
-# sequences = np.array(sequences)
-# X, y = sequences[:, :-1], sequences[:, -1]
-# sequences = [to_categorical(x, num_classes=vocab_size) for x in X]
-# X = np.array(sequences)
-# y = to_categorical(y, num_classes=vocab_size)
 
-#lines_array = np.array(lines)
 vectorize_layer.adapt(lines)
 #print(lines)
 vec_text = vectorize_layer(lines)
 lines_array = np.array(vec_text)
 input_hot = to_categorical(vec_text)
 #print(input_hot)
-test = input_hot[0].flatten()
-print(test.shape)
-print(input_hot.shape)
-print(input_hot.size)
-#flatten = input_hot.flatten()
-#print(flatten)
+
 
 t_lines_array = lines
 vectorize_layer2.adapt(t_lines_array)
@@ -68,24 +53,28 @@ t_lines_array = np.array(target_vec)
 #print(t_lines_array)
 target_hot = to_categorical(target_vec)
 #print(target_hot.shape)
-#t_flatten = target_hot.flatten()
-#print(t_flatten)
+
 
 model = tf.keras.models.Sequential()
 model.add(tf.keras.layers.Flatten(input_shape=(7, 28)))
-model.add(tf.keras.Input(shape=196))
+model.add(tf.keras.layers.Dense(196))
 model.add(tf.keras.layers.Dense(502, activation='softmax'))
-model.add(tf.keras.layers.Dense(502))
 
 print(model.summary())
 model.compile(loss='categorical_crossentropy',
               optimizer=tf.keras.optimizers.legacy.Adam(learning_rate=1e-3),
               metrics=['accuracy'],
               )
-epochs = 50
+epochs = 10
 history = model.fit(input_hot, target_hot, epochs=epochs)
 
-loss, accuracy = model.evaluate(input_hot)
-print("Loss: ", loss)
-print("Accuracy: ", accuracy)
 
+print("Evaluate model on test data")
+results = model.evaluate(input_hot, target_hot, batch_size=128)
+print("test loss, test acc:", results)
+
+test_input = input_hot[0].reshape(1, 7, 28)
+output = model(test_input)
+output = np.array(output)
+print(output)
+print(np.argmax(output))
