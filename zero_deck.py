6e429c45d7e89e9a05f5452ce2310786ecc6682c
Kiril Shenouda Khalil <kiril.shenouda-khalil@nmi.fi>
2023-04-05 14:46:22 +0300

    RD: Cleaning code.

--- zero_deck.py
+++ zero_deck.py
@@ -26,7 +26,6 @@ mapping = dict((c, i) for i, c in enumerate(chars))  # All input chars given an 
 vocab_size = len(mapping)  # Size of vocabulary
 print(vocab_size)
 
-
 vectorize_layer = layers.TextVectorization(
     standardize=None,
     split="character",
@@ -38,7 +37,6 @@ vectorize_layer2 = layers.TextVectorization(
     output_mode="int",
 )
 
-
 vectorize_layer.adapt(lines)
 print(lines)
 vec_text = vectorize_layer(lines)
@@ -47,20 +45,18 @@ lines_array = np.array(vec_text)
 input_hot = to_categorical(vec_text)
 print(input_hot)
 
-
 t_lines_array = lines
 vectorize_layer2.adapt(t_lines_array)
 target_vec = vectorize_layer2(t_lines_array)
 t_lines_array = np.array(target_vec)
-#print(t_lines_array)
+# print(t_lines_array)
 target_hot = to_categorical(target_vec)
-#print(target_hot.shape)
+# print(target_hot.shape)
 
 
 model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Flatten(input_shape=(7, 28)))
-#model.add(tf.keras.layers.Dense(196))  # Likely doing sigmoid activation with fewer neurons compared to input for One-Deck
-model.add(tf.keras.layers.Dense(502, activation='sigmoid'))  # Change to sigmoid
+model.add(layers.Flatten(input_shape=(7, 28)))
+model.add(layers.Dense(502, activation='sigmoid'))
 
 print(model.summary())
 model.compile(loss='categorical_crossentropy',
@@ -69,7 +65,6 @@ model.compile(loss='categorical_crossentropy',
               )
 epochs = 100
 history = model.fit(input_hot, target_hot, epochs=epochs)
-
 
 print("Evaluate model on test data")
 results = model.evaluate(input_hot, target_hot, batch_size=128)
