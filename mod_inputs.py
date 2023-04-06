1d3b67e98e3a91827ae71f9b0fff4d24672f234a
Kiril Shenouda Khalil <kiril.shenouda-khalil@nmi.fi>
2023-04-06 15:35:12 +0300

    RD: Created a script that takes an input file with string and then positional word (Dandurand 2013) representation for each position for said words and outputs to a file.

---
+++ mod_inputs.py
@@ -0,0 +1,32 @@
+def load_doc(filename):
+    # open the file as read only
+    file = open(filename, 'r')
+    # read all text
+    text = file.read()
+    # close the file
+    file.close()
+    return text
+
+
+def save_doc(data, filename):
+    file = open(filename, 'w')
+    file.write(data)
+    file.close()
+
+
+doc_loc = '../word_list.rtf'
+raw_text = load_doc(doc_loc)
+words = raw_text.split()
+word_length = 7
+total_length = 13
+filler_token = '#'
+new_corpus = list()
+
+for word in words:
+    for x in range(total_length - word_length + 1):
+        edited_word = filler_token * (total_length - word_length - x) + word + filler_token * x
+        new_corpus.append(edited_word)
+
+new_corpus = str(new_corpus)
+save_filename = 'positional_corpus.rtf'
+save_doc(new_corpus, save_filename)
