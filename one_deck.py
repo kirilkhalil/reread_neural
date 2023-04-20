cbddc428f836f49d7b3aa978b49939c6d52403c0
Kiril Shenouda Khalil <kiril.shenouda-khalil@nmi.fi>
2023-04-20 15:38:25 +0300

    RD: Changed one_deck to use own implementation of integer casting letters and words for easier sorting of numeral values.

--- one_deck.py
+++ one_deck.py
@@ -1,5 +1,6 @@
 import numpy as np
 import tensorflow as tf
+import weight_multiplier
 from tensorflow.keras import layers
 from keras.utils import to_categorical
 
@@ -16,14 +17,27 @@ def load_doc(filename):
     return text
 
 
-raw_text = load_doc('../positional_corpus.rtf')
-lines = raw_text.split()
-chars = sorted(list(set(raw_text)))  # All the separate chars found in input text
+raw_input_text = load_doc('../positional_corpus.rtf')
+input_lines = raw_input_text.split()
+chars = sorted(list(set(raw_input_text)))  # All the separate chars found in input text
 chars.remove('\n')
+chars.remove(' ')
 # print(chars)
 mapping = dict((c, i) for i, c in enumerate(chars))  # All input chars given an integer key value
+# print(mapping)
+sequences = list()
+for line in input_lines:
+    encoded_seq = [mapping[char] for char in line]
+    sequences.append(encoded_seq)
 vocab_size = len(mapping)  # Size of vocabulary
 # print(vocab_size)
+sequences = np.array(sequences)
+# print(sequences)
+
+
+raw_output_text = load_doc('../word_list.rtf')
+output_lines = raw_output_text.split()
+
 
 vectorize_layer = layers.TextVectorization(
     standardize=None,
@@ -36,44 +50,46 @@ vectorize_layer2 = layers.TextVectorization(
     output_mode="int",
 )
 
-vectorize_layer.adapt(lines)
+vectorize_layer.adapt(input_lines)
 # print(lines)
-vec_text = vectorize_layer(lines)
+vec_text = vectorize_layer(input_lines)
 # print(vec_text)
 lines_array = np.array(vec_text)
-input_hot = to_categorical(vec_text)
-# print(input_hot)
+lines_array[lines_array == 2] = -1
+# print(lines_array[0])
+input_hot = to_categorical(sequences, vocab_size)
+# print(input_hot[0])
+weighted_inputs = weight_multiplier.apply_input_weights(input_hot)
 
-t_lines_array = lines
-vectorize_layer2.adapt(t_lines_array)
-target_vec = vectorize_layer2(t_lines_array)
+vectorize_layer2.adapt(input_lines)
+target_vec = vectorize_layer2(input_lines)
 t_lines_array = np.array(target_vec)
 # print(t_lines_array)
-target_hot = to_categorical(target_vec)
+target_hot = to_categorical(t_lines_array)
 # print(target_hot.shape)
 
 
-model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Flatten(input_shape=(13, 29)))
-model.add(tf.keras.layers.Dense(60,
-                                activation='sigmoid'))  # Number of neurons with 500 input words rounded up to INT:
-# sqrt(500 * 7) = 60
-model.add(tf.keras.layers.Dense(3502, activation='sigmoid'))
-
-print(model.summary())
-model.compile(loss='categorical_crossentropy',
-              optimizer=tf.keras.optimizers.legacy.Adam(learning_rate=1e-3),
-              metrics=['accuracy'],
-              )
-epochs = 30
-history = model.fit(input_hot, target_hot, epochs=epochs)
-
-print("Evaluate model on test data")
-results = model.evaluate(input_hot, target_hot, batch_size=128)
-print("test loss, test acc:", results)
-
-test_input = input_hot[0].reshape(1, 13, 29)
-output = model(test_input)
-output = np.array(output)
-print(output)
-print(np.argmax(output))
+# model = tf.keras.models.Sequential()
+# model.add(tf.keras.layers.Flatten(input_shape=(13, 29)))
+# model.add(tf.keras.layers.Dense(60,
+#                                 activation='sigmoid'))  # Number of neurons with 500 input words rounded up to INT:
+# # sqrt(500 * 7) = 60
+# model.add(tf.keras.layers.Dense(3502, activation='sigmoid'))
+#
+# print(model.summary())
+# model.compile(loss='categorical_crossentropy',
+#               optimizer=tf.keras.optimizers.legacy.Adam(learning_rate=1e-3),
+#               metrics=['accuracy'],
+#               )
+# epochs = 30
+# history = model.fit(input_hot, target_hot, epochs=epochs)
+#
+# print("Evaluate model on test data")
+# results = model.evaluate(input_hot, target_hot, batch_size=128)
+# print("test loss, test acc:", results)
+#
+# test_input = input_hot[0].reshape(1, 13, 29)
+# output = model(test_input)
+# output = np.array(output)
+# print(output)
+# print(np.argmax(output))
