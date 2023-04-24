253b8001fc6d95fd0605bcb955badd7b3088d283
Kiril Shenouda Khalil <kiril.shenouda-khalil@nmi.fi>
2023-04-24 13:39:33 +0300

    RD: Code cleaning.

--- weight_multiplier.py
+++ weight_multiplier.py
@@ -14,8 +14,7 @@ class FixationMultipliers(Enum):
 def multiplication(fixation_value, mult_values, weight_array):
     for x in range(fixation_value, fixation_value + 7):
         weight_array[x][weight_array[x].nonzero()] = mult_values[x - fixation_value]
-        print(x)
-        print(weight_array[x][weight_array[x].nonzero()])
+
     return weight_array
 
 
@@ -24,31 +23,24 @@ def weight_applier(nd_array, fixation):
     if fixation == 6:  # Fixated letter = 1. Fixation-- = Fixated letter++
         multiplier = FixationMultipliers.FIRST.value
         nd_array = multiplication(fixation, multiplier, nd_array)
-        print('test 6')
     elif fixation == 5:
         multiplier = FixationMultipliers.SECOND.value
         nd_array = multiplication(fixation, multiplier, nd_array)
-        print('test 5')
     elif fixation == 4:
         multiplier = FixationMultipliers.THIRD.value
         nd_array = multiplication(fixation, multiplier, nd_array)
-        print('test 4')
     elif fixation == 3:
         multiplier = FixationMultipliers.FOURTH.value
         nd_array = multiplication(fixation, multiplier, nd_array)
-        print('test 3')
     elif fixation == 2:
         multiplier = FixationMultipliers.FIFTH.value
         nd_array = multiplication(fixation, multiplier, nd_array)
-        print('test 2')
     elif fixation == 1:
         multiplier = FixationMultipliers.SIXTH.value
         nd_array = multiplication(fixation, multiplier, nd_array)
-        print('test 1')
     elif fixation == 0:
         multiplier = FixationMultipliers.SEVENTH.value
         nd_array = multiplication(fixation, multiplier, nd_array)
-        print('test 0')
     else:
         print('ERROR')
 
@@ -75,14 +67,3 @@ def apply_input_weights(inputs):
 
     return inputs
 
-
-# Pattern that repeats for letter positions:
-# (array([ 6,  7,  8,  9, 10, 11, 12]), array([ 1,  2,  9, 12,  9, 20, 25]))
-# (array([ 5,  6,  7,  8,  9, 10, 11]), array([ 1,  2,  9, 12,  9, 20, 25]))
-# (array([ 4,  5,  6,  7,  8,  9, 10]), array([ 1,  2,  9, 12,  9, 20, 25]))
-# (array([3, 4, 5, 6, 7, 8, 9]), array([ 1,  2,  9, 12,  9, 20, 25]))
-# (array([2, 3, 4, 5, 6, 7, 8]), array([ 1,  2,  9, 12,  9, 20, 25]))
-# (array([1, 2, 3, 4, 5, 6, 7]), array([ 1,  2,  9, 12,  9, 20, 25]))
-# (array([0, 1, 2, 3, 4, 5, 6]), array([ 1,  2,  9, 12,  9, 20, 25]))
-# (array([ 6,  7,  8,  9, 10, 11, 12]), array([ 1,  2,  9, 12,  9, 20, 25]))
-# ETC.....
