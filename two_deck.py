098b39722139ca0d26af8a748bf9d3f783d3c4b6
Kiril Shenouda Khalil <kiril.shenouda-khalil@nmi.fi>
2023-05-02 15:09:09 +0300

    RD: Target ohe coded data shape is still off, need to modify further.

--- two_deck.py
+++ two_deck.py
@@ -17,15 +17,17 @@ def load_doc(filename):
 
 raw_input_text = load_doc('../positional_corpus.rtf')
 input_lines = raw_input_text.split()
+
 chars = sorted(list(set(raw_input_text)))  # All the separate chars found in input text
 chars.remove(' ')
 chars.remove('\n')
 mapping = dict((c, i) for i, c in enumerate(chars))  # All input chars given an integer key value
+vocab_size = len(mapping)  # Size of vocabulary
+
 sequences = list()
 for line in input_lines:
     encoded_seq = [mapping[char] for char in line]
     sequences.append(encoded_seq)
-vocab_size = len(mapping)  # Size of vocabulary
 sequences = np.array(sequences)
 input_hot = to_categorical(sequences, vocab_size)
 for input_matrix in input_hot:  # We don't want the char '#' to have an active bit (1) in its representation
@@ -33,16 +35,26 @@ for input_matrix in input_hot:  # We don't want the char '#' to have an active b
         if input_vector[0] == 1:
             input_vector[0] = 0
 
+raw_output_text = load_doc('two_deck_target_words.rtf')
+output_lines = raw_output_text.split()
+output_sequences = list()
+for line in output_lines:
+    encoded_seq = [mapping[char] for char in line]
+    output_sequences.append(encoded_seq)
+output_sequences = np.array(output_sequences)
+output_hot = to_categorical(output_sequences)
+print(output_hot.shape)  # Shape we are looking for is (500, 196), we have 500 words and vectors for each are 7*27 long
+
 # model = tf.keras.models.Sequential()
 # model.add(tf.keras.layers.Flatten(input_shape=(13, 27)))
 # model.add(tf.keras.layers.Dense(60,
 #                                 activation='sigmoid'))  # Number of neurons with 500 input words rounded up to INT:
 # # sqrt(500 * 7) = 60
-# model.add(tf.keras.layers.Dense(7, 27, activation='sigmoid'))
+# model.add(tf.keras.layers.Dense(189, activation='sigmoid'))
 # print(model.summary())
 # model.compile(loss='categorical_crossentropy',
 #               optimizer=tf.keras.optimizers.legacy.Adam(learning_rate=1e-3),
 #               metrics=['accuracy'],
 #               )
 # epochs = 40
-# history = model.fit(weighted_inputs, output_hot, epochs=epochs)
+# history = model.fit(input_hot, output_hot, epochs=epochs)
