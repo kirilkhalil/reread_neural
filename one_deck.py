25fe2b88d8da7f352ebe7bc2a05f0cc3d1addbd3
Kiril Shenouda Khalil <kiril.shenouda-khalil@nmi.fi>
2023-05-02 15:08:38 +0300

    RD: Cleaning code.

--- one_deck.py
+++ one_deck.py
@@ -25,74 +25,42 @@ input_lines = raw_input_text.split()
 chars = sorted(list(set(raw_input_text)))  # All the separate chars found in input text
 chars.remove(' ')
 chars.remove('\n')
-# print(chars)
 mapping = dict((c, i) for i, c in enumerate(chars))  # All input chars given an integer key value
-# print(mapping)
 sequences = list()
 for line in input_lines:
     encoded_seq = [mapping[char] for char in line]
     sequences.append(encoded_seq)
 vocab_size = len(mapping)  # Size of vocabulary
-# print(vocab_size)
 sequences = np.array(sequences)
-# print(sequences)
-
 
 raw_output_text = load_doc('../word_list.rtf')
 output_lines = raw_output_text.split()
-
-vectorize_layer = layers.TextVectorization(
-    standardize=None,
-    split="character",
-    output_mode="int",
-)
-
-vectorize_layer2 = layers.TextVectorization(
-    standardize=None,
-    output_mode="int",
-)
-
-# vectorize_layer.adapt(input_lines)
-# print(lines)
-# vec_text = vectorize_layer(input_lines)
-# print(vec_text)
-# lines_array = np.array(vec_text)
-# lines_array[lines_array == 2] = -1
-# print(lines_array[0])
 input_hot = to_categorical(sequences, vocab_size)
-# print(input_hot[0])
 weighted_inputs = weight_multiplier.apply_input_weights(input_hot)
-# print(weighted_inputs)
+print(label_lines)
 output_hot = to_categorical(label_lines)
-# print(output_hot)
-# print(output_hot.shape)
-
-vectorize_layer2.adapt(input_lines)
-target_vec = vectorize_layer2(input_lines)
-t_lines_array = np.array(target_vec)
-# print(t_lines_array)
-target_hot = to_categorical(t_lines_array)
-# print(target_hot.shape)
+print(output_hot[0])
+print(output_hot.shape)
 
 
-model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Flatten(input_shape=(13, 27)))
-model.add(tf.keras.layers.Dense(60,
-                                activation='sigmoid'))  # Number of neurons with 500 input words rounded up to INT:
-# sqrt(500 * 7) = 60
-model.add(tf.keras.layers.Dense(501, activation='sigmoid'))
-print(model.summary())
-model.compile(loss='categorical_crossentropy',
-              optimizer=tf.keras.optimizers.legacy.Adam(learning_rate=1e-3),
-              metrics=['accuracy'],
-              )
-epochs = 50
-history = model.fit(weighted_inputs, output_hot, epochs=epochs)
-print("Evaluate model on test data")
-results = model.evaluate(weighted_inputs, output_hot, batch_size=128)
-print("test loss, test acc:", results)
-test_input = weighted_inputs[0].reshape(1, 13, 27)
-output = model(test_input)
-output = np.array(output)
-print(output)
-print(np.argmax(output))
+# model = tf.keras.models.Sequential()
+# model.add(tf.keras.layers.Flatten(input_shape=(13, 27)))
+# model.add(tf.keras.layers.Dense(60,
+#                                 activation='sigmoid'))  # Number of neurons with 500 input words rounded up to INT:
+# # sqrt(500 * 7) = 60
+# model.add(tf.keras.layers.Dense(501, activation='sigmoid'))
+# print(model.summary())
+# model.compile(loss='categorical_crossentropy',
+#               optimizer=tf.keras.optimizers.legacy.Adam(learning_rate=1e-3),
+#               metrics=['accuracy'],
+#               )
+# epochs = 50
+# history = model.fit(weighted_inputs, output_hot, epochs=epochs)
+# print("Evaluate model on test data")
+# results = model.evaluate(weighted_inputs, output_hot, batch_size=128)
+# print("test loss, test acc:", results)
+# test_input = weighted_inputs[0].reshape(1, 13, 27)
+# output = model(test_input)
+# output = np.array(output)
+# print(output)
+# print(np.argmax(output))
