41495811b4a81843dc37b09214109e0929307e37
Kiril Shenouda Khalil <kiril.shenouda-khalil@nmi.fi>
2023-05-12 11:37:33 +0300

    RD: Changed loss function to mse instead cat_crossentropy.

--- two_deck.py
+++ two_deck.py
@@ -1,6 +1,7 @@
 import numpy as np
 import tensorflow as tf
 import weight_multiplier
+import output_evaluation
 from keras.utils import to_categorical
 
 np.set_printoptions(threshold=np.inf)
@@ -37,10 +38,12 @@ raw_output_text = load_doc('two_deck_target_words.rtf')
 output_lines = raw_output_text.split()
 print(output_lines)
 output_sequences = list()
+chars.remove('#')  # Don't want '#' char as one of our output alphabet
 for line in output_lines:
     encoded_seq = [mapping[char] for char in line]
     output_sequences.append(encoded_seq)
 output_sequences = np.array(output_sequences)
+print(output_sequences)
 output_hot = to_categorical(output_sequences)
 print(output_hot.shape)
 flattened_target = list()
@@ -59,17 +62,22 @@ model.add(tf.keras.layers.Dense(60,
 # input words rounded up to INT: sqrt(500 * 7) = 60
 model.add(tf.keras.layers.Dense(189, activation='sigmoid'))
 print(model.summary())
-model.compile(loss='categorical_crossentropy',
+model.compile(loss='mean_squared_error',
               optimizer=tf.keras.optimizers.legacy.SGD(learning_rate=0.9, momentum=0.2),
-              metrics=['accuracy'],
+              metrics=['mean_squared_error'],
               )
-epochs = 50
+epochs = 500
 history = model.fit(weighted_inputs, flattened_target, epochs=epochs)
-print("Evaluate model on test data")
-results = model.evaluate(weighted_inputs, flattened_target, batch_size=128)
-print("test loss, test acc:", results)
-test_input = weighted_inputs[0].reshape(1, 13, 27)
-output = model(test_input)
+# print("Evaluate model on test data")
+# results = model.evaluate(weighted_inputs, flattened_target, batch_size=128)
+# print("test loss, test acc:", results)
+test_input = weighted_inputs[0].reshape(1, 13, 27)  # ######ABILITY
+output = model(test_input)  # Expected output is: ABILITY
 output = np.array(output)
-print(output)
-print(np.argmax(output))
+# print(output)
+# print(output.shape)
+# print(np.argmax(output))
+output_evaluation.output_eval(output)
+
+
+
