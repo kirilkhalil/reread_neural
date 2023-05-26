313681a5b2570f212a6f08637d568f52b6b1e860
Kiril Shenouda Khalil <kiril.shenouda-khalil@nmi.fi>
2023-05-26 11:45:05 +0300

    RD: Set seed to contant. Made word length and vocab determination for model dynamic based on give inputs.

--- two_deck.py
+++ two_deck.py
@@ -6,6 +6,9 @@ from keras.utils import to_categorical
 from pickle import dump
 
 
+tf.keras.utils.set_random_seed(
+    24
+)
 np.set_printoptions(threshold=np.inf)
 
 
@@ -21,6 +24,7 @@ def load_doc(filename):
 
 raw_input_text = load_doc('../positional_corpus.rtf')
 input_lines = raw_input_text.split()
+print(input_lines)
 
 chars = sorted(list(set(raw_input_text)))  # All the separate chars found in input text
 chars.remove(' ')
@@ -29,7 +33,10 @@ mapping = dict((c, i) for i, c in enumerate(chars))  # All input chars given an 
 vocab_size = len(mapping)  # Size of vocabulary
 
 sequences = list()
+word_length = 0
 for line in input_lines:
+    if len(line) > word_length:  # Figure out the longest input word length. Used also for padding length if needed.
+        word_length = len(line)
     encoded_seq = [mapping[char] for char in line]
     sequences.append(encoded_seq)
 sequences = np.array(sequences)
@@ -58,28 +65,30 @@ print(flattened_target.shape)
 
 initializer = tf.keras.initializers.RandomUniform(minval=-0.5, maxval=0.5)
 model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Flatten(input_shape=(13, 27)))
-model.add(tf.keras.layers.Dense(60,
-                                activation='sigmoid', kernel_initializer=initializer))  # Number of neurons with 500
+model.add(tf.keras.layers.Flatten(input_shape=(word_length, vocab_size)))
+model.add(tf.keras.layers.Dense(120,
+                                activation='sigmoid', kernel_initializer=initializer, ))  # Number of neurons with 500
 # input words rounded up to INT: sqrt(500 * 7) = 60
 model.add(tf.keras.layers.Dense(189, activation='sigmoid'))
 print(model.summary())
-model.compile(loss='mean_squared_error',
-              optimizer=tf.keras.optimizers.legacy.SGD(learning_rate=0.9, momentum=0.2),
+model.compile(loss=tf.keras.losses.MeanSquaredError(),
+              optimizer=tf.keras.optimizers.legacy.SGD(learning_rate=200, momentum=0.2),
               metrics=['mean_squared_error'],
               )
-epochs = 20000
+epochs = 800
 history = model.fit(weighted_inputs, flattened_target, epochs=epochs)
 # print("Evaluate model on test data")
 # results = model.evaluate(weighted_inputs, flattened_target, batch_size=128)
 # print("test loss, test acc:", results)
-test_input = weighted_inputs[0].reshape(1, 13, 27)  # ######ABILITY
-output = model(test_input)  # Expected output is: ABILITY
-output = np.array(output)
+for x in range(0, 49):
+    test_input = weighted_inputs[x].reshape(1, word_length, vocab_size)
+    output = model(test_input)
+    output = np.array(output)
+    output_evaluation.output_eval(output)
 # print(output)
 # print(output.shape)
 # print(np.argmax(output))
-output_evaluation.output_eval(output)
+# output_evaluation.output_eval(output)
 model.save('lower_deck.h5')
 dump(mapping, open('lower_deck_mapping.pkl', 'wb'))
 
