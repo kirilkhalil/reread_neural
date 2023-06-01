6842495456c3bd7382a103c2bf5b2eb5ed4b94d6
Kiril Shenouda Khalil <kiril.shenouda-khalil@nmi.fi>
2023-06-01 13:04:41 +0300

    RD: Now have a print out in the form of {input_word: output_lower_deck, output_upper_deck}.

--- two_deck.py
+++ two_deck.py
@@ -20,12 +20,12 @@ def load_doc(filename):
     return text
 
 
+input_output_dict = {}
 lower_deck_model = load_model('lower_deck.h5')
 lower_deck_mapping = load(open('lower_deck_mapping.pkl', 'rb'))
-print(lower_deck_mapping)
 raw_input_text = load_doc('../positional_corpus.rtf')
 lower_deck_raw_input_lines = raw_input_text.split()
-lower_deck_raw_inputs = lower_deck_raw_input_lines[0:7]  # Words change every 7 indexes.
+lower_deck_raw_inputs = lower_deck_raw_input_lines[0:35]  # Words change every 7 indexes.
 lower_deck_vocab_size = len(lower_deck_mapping)  # Size of vocabulary
 
 lower_deck_sequences = list()
@@ -33,7 +33,6 @@ for word in lower_deck_raw_inputs:
     encoded_seq = [lower_deck_mapping[char] for char in word]
     lower_deck_sequences.append(encoded_seq)
 lower_deck_sequences = np.array(lower_deck_sequences)
-print(lower_deck_sequences)
 lower_deck_input_hot = to_categorical(lower_deck_sequences, lower_deck_vocab_size)
 weighted_inputs = weight_multiplier.apply_input_weights(lower_deck_input_hot)
 lower_deck_outputs_str = list()
@@ -41,25 +40,25 @@ for x in range(len(weighted_inputs)):
     lower_deck_input = weighted_inputs[x].reshape(1, 13, 27)
     lower_deck_output = lower_deck_model.predict(lower_deck_input)
     lower_deck_output = np.array(lower_deck_output)
-    # print(lower_deck_output)
-    # print(lower_deck_output.shape)  # Output is an array of 189 in length. Need to pick the predicted chars and ohe them again.
     lower_deck_outputs_str.append(output_evaluation.output_eval(lower_deck_output))
-    # print(lower_deck_outputs_int)  # INT representation of predicted word. Recast to ohe and feed to upper deck.
 
 upper_deck_model = load_model('upper_deck.h5')
 upper_deck_mapping = load(open('upper_deck_mapping.pkl', 'rb'))
-print(upper_deck_mapping)
 upper_deck_vocab_size = len(upper_deck_mapping)
 upper_deck_sequences = list()
+upper_deck_outputs = list()
 for output_word in lower_deck_outputs_str:
     encoded_seq = [upper_deck_mapping[char] for char in output_word]
     upper_deck_sequences.append(encoded_seq)
 upper_deck_sequences = np.array(upper_deck_sequences)
-print(upper_deck_sequences)
 upper_deck_input_hot = to_categorical(upper_deck_sequences, upper_deck_vocab_size)
-print(upper_deck_input_hot)
 for j in range(len(upper_deck_input_hot)):
     upper_deck_input = upper_deck_input_hot[j].reshape(1, 7, 26)
     upper_deck_output = upper_deck_model.predict(upper_deck_input)
-    print(upper_deck_output.shape)
-    print(np.argmax(upper_deck_output))
+    upper_deck_outputs.append(np.argmax(upper_deck_output))
+print(lower_deck_raw_inputs)
+print(lower_deck_outputs_str)
+print(upper_deck_outputs)
+for i in range(len(upper_deck_outputs)):
+    input_output_dict[lower_deck_raw_inputs[i]] = lower_deck_outputs_str[i], upper_deck_outputs[i]
+print(input_output_dict)
