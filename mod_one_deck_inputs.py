c480ca6b1adf78c4e6cae49dca5961b0be689227
Kiril Shenouda Khalil <kiril.shenouda-khalil@nmi.fi>
2023-06-12 14:07:21 +0300

    RD: Codec changes to handle special characters (French / Finnish)

--- mod_one_deck_inputs.py
+++ mod_one_deck_inputs.py
@@ -1,6 +1,9 @@
+import codecs as c
+
+
 def load_doc(filename):
     # open the file as read only
-    file = open(filename, 'r')
+    file = c.open(filename, 'r', encoding='utf-16')
     # read all text
     text = file.read()
     # close the file
@@ -9,12 +12,12 @@ def load_doc(filename):
 
 
 def save_doc(data, filename):
-    file = open(filename, 'w')
+    file = c.open(filename, 'w', encoding='utf-16')
     file.write(data)
     file.close()
 
 
-doc_loc = '../word_list.rtf'
+doc_loc = '../french_corpus.txt'
 raw_text = load_doc(doc_loc)
 words = raw_text.split()
 word_length = 7
@@ -35,7 +38,7 @@ save_file = " ".join(new_corpus)
 label_file = " ".join(label_list)
 print(label_list)
 print(save_file)
-save_filename = 'positional_supervised_corpus.rtf'
-save_labelname = 'labels.rtf'
+save_filename = 'french_positional_supervised_corpus.txt'
+save_labelname = 'french_labels.txt'
 save_doc(save_file, save_filename)
 save_doc(label_file, save_labelname)
--- mod_two_deck_target_words.py
+++ mod_two_deck_target_words.py
@@ -1,6 +1,9 @@
+import codecs as c
+
+
 def load_doc(filename):
     # open the file as read only
-    file = open(filename, 'r')
+    file = c.open(filename, 'r', encoding='utf-16')
     # read all text
     text = file.read()
     # close the file
@@ -9,14 +12,15 @@ def load_doc(filename):
 
 
 def save_doc(data, filename):
-    file = open(filename, 'w')
+    file = c.open(filename, 'w', encoding='utf-16')
     file.write(data)
     file.close()
 
 
-doc_loc = '../word_list.rtf'
+doc_loc = '../french_corpus.txt'
 raw_text = load_doc(doc_loc)
 words = raw_text.split()
+print(len(words))
 new_corpus = list()
 
 for word in words:
@@ -25,5 +29,5 @@ for word in words:
 
 save_file = " ".join(new_corpus)
 print(save_file)
-save_filename = 'two_deck_target_words.rtf'
+save_filename = 'french_two_deck_target_words.txt'
 save_doc(save_file, save_filename)
